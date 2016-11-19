424e14f47d61991ee6ad703d542db7e7e984c7e7
kwe <kwevans@gmail.com>
2016-11-19 14:20:50 +0000

    initial commit

---
+++ Post.cs
@@ -0,0 +1,8 @@
+namespace ConsoleApplication
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public string Response { get; set; }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Post post = new Post();
+            HttpClient client = new HttpClient();
+            post = GetApi(client).Result;
+
+        }
+        static async Task<Post> GetApi(HttpClient c)
+        {
+            Post post = new Post();
+            
+            var r = await c.GetStringAsync("http://localhost:8000/wp-json/wp/v2/posts");
+            
+            post.Title = "Set in Task";
+            post.Content = "Hey now";
+
+            return post;
+        }
+    }
+}
